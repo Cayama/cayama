cd69f286b25ac5b7808593e1951d48896cf11efb
Jafet Henrique <jafetguerra6@gmail.com>
2020-08-11 10:12:14 -0300

    estruturando página com css module para cada pagina

--- frontend/src/pages/HomePage/Advantages.js
+++ frontend/src/pages/HomePage/Advantages.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
+import HomePageClientCss from './HomePageClient.module.css';
 
 function Advantages() {
   const advantages = useSelector(state => state.homePageReducer.advantages)
   return (
     <div>
       {advantages.map(({ advantage }) => (
-        <div key={advantage}>
+        <div className={HomePageClientCss.advantageBox} key={advantage}>
           {advantage}
         </div>
       ))}
--- frontend/src/pages/HomePage/HomePageClient.js
+++ frontend/src/pages/HomePage/HomePageClient.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Carousel from './Carousel';
 import Advantages from './Advantages';
 import ProductCardContainer from '../../components/ProductCardContainer';
+import HomePageClientCss from './HomePageClient.module.css';
 
 const HomePageClient = () => {
   return (
     <div>
       <Carousel />
-      <Advantages />
-      <ProductCardContainer />
+      <div className={HomePageClientCss.contentContainer}>
+        <Advantages />
+        <ProductCardContainer />
+      </div>
     </div>
   )
 }
