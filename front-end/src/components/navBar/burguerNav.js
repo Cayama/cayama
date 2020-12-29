bc2c6d897913dd1e470202f94537ebc96ed91aa9
Jafet Henrique <jafetguerra6@gmail.com>
2020-12-29 14:55:10 -0300

    menu hamburguer funcionando abrindo e fechando ao clinar nos links

--- front-end/src/components/navBar/burguerNav.js
+++ front-end/src/components/navBar/burguerNav.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { StyledBurguer } from './styles';
 import { hamburgerMenuAction } from '../../redux/action/hamburgerMenuAction';
 
 const BurguerNav = () => {
   const [open, setOpen] = useState(false);
+  const reducerOpenStatus = useSelector((state) => state.hamburgerMenuReducer.status)
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    setOpen(reducerOpenStatus)
+  }, [reducerOpenStatus])
+
   return (
     <StyledBurguer open={open} onClick={() => {
       dispatch(hamburgerMenuAction(!open));
--- front-end/src/components/navBar/smallerScreen/menuSmallerScreen.js
+++ front-end/src/components/navBar/smallerScreen/menuSmallerScreen.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Li from '../../layout/liGroup';
+import { useDispatch } from 'react-redux';
 import navBarStructure from '../navBarSettings/navBarStructure';
 import Link from '../../../infra/components/link';
+import { hamburgerMenuAction } from '../../../redux/action/hamburgerMenuAction';
 
 function MenuSmallerScreen({ open }) {
+  const dispatch = useDispatch()
   return (
     <>
       {open ? (navBarStructure.map(({ url, name }) =>
-      <Li>
-        <Link key={name} href={url}>
+      <Li key={name}>
+        <Link onClick={() => dispatch(hamburgerMenuAction(!open))} href={url}>
           {name}
         </Link>
       </Li>
